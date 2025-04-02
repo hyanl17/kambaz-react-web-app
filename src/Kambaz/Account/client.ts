0144c29ea550cd0f050b09cbe506cd9a5a109e03
Huiyan Lei <huiyanlei@Huiyans-MacBook-Pro.local>
2025-04-02 14:19:08 -0400

    update

--- src/Kambaz/Account/client.ts
+++ src/Kambaz/Account/client.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosWithCredentials = axios.create({ withCredentials: true });
 export const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
-export const USERS_API = `${REMOTE_SERVER}api/users`;
-export const ENROLLMENTS_API = `${REMOTE_SERVER}api/enrollments`;
+export const USERS_API = `${REMOTE_SERVER}/api/users`;
+export const ENROLLMENTS_API = `${REMOTE_SERVER}/api/enrollments`;
 export const signin = async (credentials: any) => {
   const response = await axiosWithCredentials.post(`${USERS_API}/signin`, credentials);
   return response.data;
